fef505f0fa209174ee6aec9f70153a864337ac95
Harsh Goel <harshgoel2004@gmail.com>
2024-07-05 15:51:00 +0530

    dep no

--- hooks/useBot.tsx
+++ hooks/useBot.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { auth } from "@/auth"
 import { getUserByEmail } from "@/helpers/get-user"
 import { Bot, User } from "@prisma/client"
 
